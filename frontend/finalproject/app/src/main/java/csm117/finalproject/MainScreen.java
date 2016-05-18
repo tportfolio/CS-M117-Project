20a4600259f254f9b90dec57a15b710c76de82cd
whatahappyday <yccvivo@gmail.com>
2016-05-17 22:29:20 -0700

    Add basic layouts

--- frontend/finalproject/app/src/main/java/csm117/finalproject/MainScreen.java
+++ frontend/finalproject/app/src/main/java/csm117/finalproject/MainScreen.java
@@ -1,7 +1,7 @@
 package csm117.finalproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainScreen extends AppCompatActivity {
 
